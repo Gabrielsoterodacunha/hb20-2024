1e189f6c7ecc7256514df7897ceddb177c4faa0f
Gabriel hub <165857910+Gabrielsoterodacunha@users.noreply.github.com>
2026-02-02 23:41:30 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,32 @@
 const initialData = {
-  "8105":"available","0739":"available","6179":"available","2997":"available",
-  "1247":"available","0051":"available","5916":"available","0542":"available",
-  "4734":"available","1816":"available","4753":"available","3222":"available",
-  "7164":"available","2105":"available","7765":"available","9690":"available",
-  "4107":"available","7504":"available","0390":"available","9541":"available",
-  "6616":"available","8262":"available","8618":"available","2391":"available",
-  "5878":"available","3079":"available","3235":"available","4739":"available",
-  "7574":"available","9973":"available"
+  "8105":"available",
+  "0739":"available",
+  "6179":"paid",      // 🔴
+  "2997":"available",
+  "1247":"available",
+  "0051":"paid",      // 🔴
+  "5916":"available",
+  "0542":"available",
+  "4734":"available",
+  "1816":"available",
+  "4753":"available",
+  "3222":"available",
+  "7164":"available",
+  "2105":"available",
+  "7765":"available",
+  "9690":"available",
+  "4107":"available",
+  "7504":"paid",      // 🔴
+  "0390":"available",
+  "9541":"available",
+  "6616":"available",
+  "8262":"available",
+  "8618":"available",
+  "2391":"available",
+  "5878":"available",
+  "3079":"available",
+  "3235":"available",
+  "4739":"available",
+  "7574":"paid",      // 🔴
+  "9973":"available"
 };
